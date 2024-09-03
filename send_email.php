f99ce2878eaef86419a1aadecfd943177eabf8a3
RifatBinSiraj <rifatrabbi024@gmail.com>
2024-09-03 21:54:08 +0600

    first

--- send_email.php
+++ send_email.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
